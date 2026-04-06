5902c19e427dbd00dde7777e59ba1809385481f6
Gaspar <gaspar.deutsch@gmail.com>
2026-04-06 22:39:31 +0200

    :wrench: add swipper

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 // Parallax.js
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene, {
-  invertX: false,
-  invertY: false
+    invertX: false,
+    invertY: false
+});
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
