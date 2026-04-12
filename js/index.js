41f9df7d37647a4a3867c46251163fcd1660d3da
Gaspar <gaspar.deutsch@gmail.com>
2026-04-12 02:07:40 +0200

    :wrench: working on slider

--- js/index.js
+++ js/index.js
@@ -6,9 +6,28 @@ var parallaxInstance = new Parallax(scene, {
 });
 
 // Swiper
-var swiper = new Swiper(".mySwiper", {
+const sectionSlider = document.querySelector('.section-slider');
+
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    speed: 500,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.btn-next',
+        prevEl: '.btn-prev',
     },
+    on: {
+        // Cette fonction se déclenche à chaque fois que la slide change
+        slideChange: function () {
+            // Récupère la slide actuellement visible
+            const activeSlide = this.slides[this.activeIndex];
+            
+            // Récupère la couleur que tu as mise dans le HTML
+            const newColor = activeSlide.getAttribute('data-bg');
+            
+            // Change la couleur de la section globale
+            if (newColor) {
+                sectionSlider.style.backgroundColor = newColor;
+            }
+        }
+    }
 });
