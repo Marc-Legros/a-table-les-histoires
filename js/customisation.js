343f3fcf62ed53b819db8db3d378c9a5544f260e
Marc Legros <auflo2007@gmail.com>
2026-04-20 11:16:30 +0200

    feat: add hand SVG asset and integrate into customization options

--- js/customisation.js
+++ js/customisation.js
@@ -1,13 +1,18 @@
-// ---- URL PARAMS ----
+// =============================
+// QUEL THÈME EST ACTIF ?
+// =============================
 const searchParams = new URLSearchParams(window.location.search);
+const themeId = searchParams.get("theme") || "theme-nuage";
 
-// ---- VARIABLES ----
+// =============================
+// ON RETIENT CE QUI EST SÉLECTIONNÉ
+// =============================
 let avatarActuel = "";
 let accessoireActuel = "";
 
-// ---- CONFIG DES THÈMES ----
-const themeId = searchParams.get("theme") || "theme-nuage";
-
+// =============================
+// TOUS LES ASSETS PAR THÈME
+// =============================
 const themes = {
     "theme-nuage": {
         couleur: "#A89FD8",
@@ -35,6 +40,7 @@ const themes = {
             "../assets/customisation/accessoires/t-shirt/t-shirt5.svg",
             "../assets/customisation/accessoires/t-shirt/t-shirt6.svg",
         ],
+        bras: "../assets/customisation/avatar/hand.svg",
         accessoires: [
             "../assets/customisation/accessoires/objets/accessoire1.svg",
             "../assets/customisation/accessoires/objets/accessoire2.svg",
@@ -70,6 +76,7 @@ const themes = {
             "../assets/customisation/accessoires/t-shirt/fee/t shirt 6 1(1).svg",
             "../assets/customisation/accessoires/t-shirt/fee/t shirt 6 1(2).svg",
         ],
+        bras: "../assets/customisation/avatar/hand.svg",
         accessoires: [
             "../assets/customisation/accessoires/objets/fee/baguette.svg",
             "../assets/customisation/accessoires/objets/fee/baguette(1).svg",
@@ -79,87 +86,14 @@ const themes = {
             "../assets/customisation/accessoires/objets/fee/Calque_1(6).svg",
         ],
     },
-    // Ajoute les autres thèmes quand tu auras les assets
-    "theme-dragon": { 
-        couleur: "#A5DB76", 
-        avatars: [
-            "../assets/customisation/avatar/Group.svg",
-            "../assets/customisation/avatar/Group1.svg",
-            "../assets/customisation/avatar/Group2.svg",
-            "../assets/customisation/avatar/Group3.svg",
-            "../assets/customisation/avatar/Group4.svg",
-            "../assets/customisation/avatar/Group5.svg",
-        ], 
-        chapeaux: [
-            "../assets/customisation/accessoires/chapeau/chevalier/chapeau de fou.svg",
-            "../assets/customisation/accessoires/chapeau/chevalier/chapeau.svg",
-            "../assets/customisation/accessoires/chapeau/chevalier/chapeau1.svg",
-            "../assets/customisation/accessoires/chapeau/chevalier/couronne(1).svg",
-            "../assets/customisation/accessoires/chapeau/chevalier/couronne2.svg",
-            "../assets/customisation/accessoires/chapeau/chevalier/couronne3.svg",
-        ], 
-        tshirts: [
-            "../assets/customisation/accessoires/t-shirt/chevalier/Calque_1(10).svg",
-            "../assets/customisation/accessoires/t-shirt/chevalier/Calque_1(7).svg",
-            "../assets/customisation/accessoires/t-shirt/chevalier/Calque_1(8).svg",
-            "../assets/customisation/accessoires/t-shirt/chevalier/Calque_1(9).svg",
-            "../assets/customisation/accessoires/t-shirt/chevalier/plastron.svg",
-            "../assets/customisation/accessoires/t-shirt/chevalier/t shirt 1 1(2).svg",
-            
-        ], 
-        accessoires: [
-            "../assets/customisation/accessoires/objets/chevalier/bouclier 1.svg",
-            "../assets/customisation/accessoires/objets/chevalier/bouclier 2.svg",
-            "../assets/customisation/accessoires/objets/chevalier/bouclier 3.svg",
-            "../assets/customisation/accessoires/objets/chevalier/Group(25).svg",
-            "../assets/customisation/accessoires/objets/chevalier/lance.svg",
-            "../assets/customisation/accessoires/objets/chevalier/épée.svg",
-        ] 
-    },
-    "theme-jungle": { 
-        couleur: "#FF8411", 
-        avatars: [
-            "../assets/customisation/avatar/Group.svg",
-            "../assets/customisation/avatar/Group1.svg",
-            "../assets/customisation/avatar/Group2.svg",
-            "../assets/customisation/avatar/Group3.svg",
-            "../assets/customisation/avatar/Group4.svg",
-            "../assets/customisation/avatar/Group5.svg",
-        ], 
-        chapeaux: [
-            "../assets/customisation/accessoires/chapeau/jungle/Calque 1(6).svg",
-            "../assets/customisation/accessoires/chapeau/jungle/casquette.svg",
-            "../assets/customisation/accessoires/chapeau/jungle/chapeau 3(1).svg",
-            "../assets/customisation/accessoires/chapeau/jungle/chapeau 3.svg",
-            "../assets/customisation/accessoires/chapeau/jungle/chapeau 6 1(3).svg",
-            "../assets/customisation/accessoires/chapeau/jungle/chapeau1(1).svg",
-        ], 
-        tshirts: [
-            
-        ], 
-        accessoires: [] 
-    },
-    "theme-espace": { 
-        couleur: "#74B5FF", 
-        avatars: [
-            "../assets/customisation/avatar/Group.svg",
-            "../assets/customisation/avatar/Group1.svg",
-            "../assets/customisation/avatar/Group2.svg",
-            "../assets/customisation/avatar/Group3.svg",
-            "../assets/customisation/avatar/Group4.svg",
-            "../assets/customisation/avatar/Group5.svg",
-        ], 
-        chapeaux: [], 
-        tshirts: [], 
-        accessoires: [] 
-    },
 };
 
+// On récupère le thème actif
 const theme = themes[themeId] || themes["theme-nuage"];
 
-
-
-// ---- CHARGEMENT DU THÈME ----
+// =============================
+// ON REMPLIT LES BOUTONS DE CHOIX
+// =============================
 function chargerTheme() {
     // Couleur du panel
     document.querySelector(".customisation").style.background = theme.couleur;
@@ -195,7 +129,89 @@ function chargerTheme() {
 
 chargerTheme();
 
-// ---- PLACER ACCESSOIRE ----
+// =============================
+// QUAND ON CLIQUE SUR UN AVATAR
+// =============================
+function cliquerAvatar(indexDansCate, src) {
+    const avatar = document.getElementById("calque-avatar");
+    avatar.src = src;
+    avatarActuel = src;
+
+    // Position de l'avatar selon lequel on a choisi
+    if (indexDansCate === 0) { avatar.style.top = "100px"; avatar.style.left = "60px"; avatar.style.width = "180px"; }
+    else if (indexDansCate === 1) { avatar.style.top = "100px"; avatar.style.left = "55px"; avatar.style.width = "190px"; }
+    else if (indexDansCate === 2) { avatar.style.top = "110px"; avatar.style.left = "70px"; avatar.style.width = "170px"; }
+    else if (indexDansCate === 3) { avatar.style.top = "100px"; avatar.style.left = "60px"; avatar.style.width = "180px"; }
+    else if (indexDansCate === 4) { avatar.style.top = "135px"; avatar.style.left = "65px"; avatar.style.width = "175px"; }
+    else if (indexDansCate === 5) { avatar.style.top = "110px"; avatar.style.left = "65px"; avatar.style.width = "165px"; }
+
+    // On affiche les bras automatiquement
+    const bras = document.getElementById("calque-bras");
+    if (bras && theme.bras) {
+        bras.src = theme.bras;
+        bras.style.top = "150px";
+        bras.style.left = "48px";
+        bras.style.width = "70px";
+    }
+
+    // On repositionne l'accessoire si il y en a un
+    placerAccessoire();
+}
+
+// =============================
+// QUAND ON CLIQUE SUR UN CHAPEAU
+// =============================
+function cliquerChapeau(indexDansCate, src) {
+    const chapeau = document.getElementById("calque-chapeau");
+    chapeau.src = src;
+
+    if (indexDansCate === 0) { chapeau.style.top = "75px"; chapeau.style.left = "38px"; chapeau.style.width = "230px"; }
+    else if (indexDansCate === 1) { chapeau.style.top = "90px"; chapeau.style.left = "69px"; chapeau.style.width = "235px"; }
+    else if (indexDansCate === 2) { chapeau.style.top = "80px"; chapeau.style.left = "38px"; chapeau.style.width = "230px"; }
+    else if (indexDansCate === 3) { chapeau.style.top = "80px"; chapeau.style.left = "30px"; chapeau.style.width = "245px"; }
+    else if (indexDansCate === 4) { chapeau.style.top = "63px"; chapeau.style.left = "73px"; chapeau.style.width = "230px"; }
+    else if (indexDansCate === 5) { chapeau.style.top = "55px"; chapeau.style.left = "23px"; chapeau.style.width = "260px"; }
+}
+
+// =============================
+// QUAND ON CLIQUE SUR UN T-SHIRT
+// =============================
+function cliquerTshirt(indexDansCate, src) {
+    const tshirt = document.getElementById("calque-tshirt");
+    tshirt.src = src;
+
+    if (indexDansCate === 0) { tshirt.style.top = "242px"; tshirt.style.left = "40px"; tshirt.style.width = "220px"; }
+    else if (indexDansCate === 1) { tshirt.style.top = "270px"; tshirt.style.left = "40px"; tshirt.style.width = "210px"; }
+    else if (indexDansCate === 2) { tshirt.style.top = "275px"; tshirt.style.left = "45px"; tshirt.style.width = "210px"; }
+    else if (indexDansCate === 3) { tshirt.style.top = "260px"; tshirt.style.left = "50px"; tshirt.style.width = "190px"; }
+    else if (indexDansCate === 4) { tshirt.style.top = "275px"; tshirt.style.left = "50px"; tshirt.style.width = "210px"; }
+    else if (indexDansCate === 5) { tshirt.style.top = "280px"; tshirt.style.left = "45px"; tshirt.style.width = "210px"; }
+
+    // On repositionne les bras selon le t-shirt
+    const bras = document.getElementById("calque-bras");
+    if (bras && bras.src) {
+        if (indexDansCate === 0) { bras.style.top = "242px"; bras.style.left = "40px"; bras.style.width = "220px"; }
+        else if (indexDansCate === 1) { bras.style.top = "270px"; bras.style.left = "40px"; bras.style.width = "210px"; }
+        else if (indexDansCate === 2) { bras.style.top = "275px"; bras.style.left = "45px"; bras.style.width = "210px"; }
+        else if (indexDansCate === 3) { bras.style.top = "260px"; bras.style.left = "50px"; bras.style.width = "190px"; }
+        else if (indexDansCate === 4) { bras.style.top = "275px"; bras.style.left = "50px"; bras.style.width = "210px"; }
+        else if (indexDansCate === 5) { bras.style.top = "280px"; bras.style.left = "45px"; bras.style.width = "210px"; }
+    }
+}
+
+// =============================
+// QUAND ON CLIQUE SUR UN ACCESSOIRE
+// =============================
+function cliquerAccessoire(src) {
+    const accessoire = document.getElementById("calque-accessoire");
+    accessoire.src = src;
+    accessoireActuel = src;
+    placerAccessoire();
+}
+
+// =============================
+// PLACER L'ACCESSOIRE AU BON ENDROIT
+// =============================
 function placerAccessoire() {
     if (!accessoireActuel) return;
 
@@ -204,20 +220,37 @@ function placerAccessoire() {
 
     const i = theme.accessoires.indexOf(accessoireActuel.replace(window.location.origin, "").replace(/^\//, "../"));
 
-    if (i === 0) { accessoire.style.top = "170px"; accessoire.style.left = "185px"; accessoire.style.width = "100px"; }
-    else if (i === 1) { accessoire.style.top = "170px"; accessoire.style.left = "180px"; accessoire.style.width = "100px"; }
-    else if (i === 2) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
-    else if (i === 3) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
-    else if (i === 4) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
-    else if (i === 5) {
-        accessoire.style.transform = "rotate(45deg)";
-        accessoire.style.top = "300px";
-        accessoire.style.left = "150px";
-        accessoire.style.width = "80px";
+    if (themeId === "theme-nuage") {
+        if (i === 0) { accessoire.style.top = "170px"; accessoire.style.left = "185px"; accessoire.style.width = "100px"; }
+        else if (i === 1) { accessoire.style.top = "170px"; accessoire.style.left = "180px"; accessoire.style.width = "100px"; }
+        else if (i === 2) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 3) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 4) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 5) {
+            accessoire.style.transform = "rotate(45deg)";
+            accessoire.style.top = "300px";
+            accessoire.style.left = "150px";
+            accessoire.style.width = "80px";
+        }
+    }
+    else if (themeId === "theme-fees") {
+        if (i === 0) { accessoire.style.top = "200px"; accessoire.style.left = "150px"; accessoire.style.width = "80px"; }
+        else if (i === 1) { accessoire.style.top = "200px"; accessoire.style.left = "150px"; accessoire.style.width = "80px"; }
+        else if (i === 2) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 3) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 4) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+        else if (i === 5) { accessoire.style.top = "200px"; accessoire.style.left = "50px"; accessoire.style.width = "100px"; }
+    }
+    else {
+        accessoire.style.top = "200px";
+        accessoire.style.left = "50px";
+        accessoire.style.width = "100px";
     }
 }
 
-// ---- CLICS ----
+// =============================
+// ON ÉCOUTE TOUS LES CLICS
+// =============================
 const btns = document.querySelectorAll("img.btn");
 const cates = document.querySelectorAll(".cate");
 
@@ -227,60 +260,17 @@ btns.forEach((btn) => {
         const cateParent = btn.closest(".cate");
         const indexDansCate = Array.from(btn.closest(".choices").querySelectorAll("img")).indexOf(btn);
 
-        // ---- AVATARS ----
-        if (cateParent === cates[0]) {
-            const avatar = document.getElementById("calque-avatar");
-            avatar.src = btn.src;
-            avatarActuel = btn.src;
-
-            if (indexDansCate === 0) { avatar.style.top = "100px"; avatar.style.left = "60px"; avatar.style.width = "180px"; }
-            else if (indexDansCate === 1) { avatar.style.top = "100px"; avatar.style.left = "55px"; avatar.style.width = "190px"; }
-            else if (indexDansCate === 2) { avatar.style.top = "110px"; avatar.style.left = "70px"; avatar.style.width = "170px"; }
-            else if (indexDansCate === 3) { avatar.style.top = "100px"; avatar.style.left = "60px"; avatar.style.width = "180px"; }
-            else if (indexDansCate === 4) { avatar.style.top = "135px"; avatar.style.left = "65px"; avatar.style.width = "175px"; }
-            else if (indexDansCate === 5) { avatar.style.top = "110px"; avatar.style.left = "65px"; avatar.style.width = "165px"; }
-
-            placerAccessoire();
-        }
-
-        // ---- CHAPEAUX ----
-        else if (cateParent === cates[1]) {
-            const chapeau = document.getElementById("calque-chapeau");
-            chapeau.src = btn.src;
-
-            if (indexDansCate === 0) { chapeau.style.top = "75px"; chapeau.style.left = "38px"; chapeau.style.width = "230px"; }
-            else if (indexDansCate === 1) { chapeau.style.top = "90px"; chapeau.style.left = "69px"; chapeau.style.width = "235px"; }
-            else if (indexDansCate === 2) { chapeau.style.top = "80px"; chapeau.style.left = "38px"; chapeau.style.width = "230px"; }
-            else if (indexDansCate === 3) { chapeau.style.top = "80px"; chapeau.style.left = "30px"; chapeau.style.width = "245px"; }
-            else if (indexDansCate === 4) { chapeau.style.top = "63px"; chapeau.style.left = "73px"; chapeau.style.width = "230px"; }
-            else if (indexDansCate === 5) { chapeau.style.top = "55px"; chapeau.style.left = "23px"; chapeau.style.width = "260px"; }
-        }
-
-        // ---- T-SHIRTS ----
-        else if (cateParent === cates[2]) {
-            const tshirt = document.getElementById("calque-tshirt");
-            tshirt.src = btn.src;
-
-            if (indexDansCate === 0) { tshirt.style.top = "242px"; tshirt.style.left = "40px"; tshirt.style.width = "220px"; }
-            else if (indexDansCate === 1) { tshirt.style.top = "270px"; tshirt.style.left = "40px"; tshirt.style.width = "210px"; }
-            else if (indexDansCate === 2) { tshirt.style.top = "275px"; tshirt.style.left = "45px"; tshirt.style.width = "210px"; }
-            else if (indexDansCate === 3) { tshirt.style.top = "260px"; tshirt.style.left = "50px"; tshirt.style.width = "190px"; }
-            else if (indexDansCate === 4) { tshirt.style.top = "275px"; tshirt.style.left = "50px"; tshirt.style.width = "210px"; }
-            else if (indexDansCate === 5) { tshirt.style.top = "280px"; tshirt.style.left = "45px"; tshirt.style.width = "210px"; }
-        }
-
-        // ---- ACCESSOIRES ----
-        else if (cateParent === cates[3]) {
-            const accessoire = document.getElementById("calque-accessoire");
-            accessoire.src = btn.src;
-            accessoireActuel = btn.src;
-            placerAccessoire();
-        }
+        if (cateParent === cates[0]) { cliquerAvatar(indexDansCate, btn.src); }
+        else if (cateParent === cates[1]) { cliquerChapeau(indexDansCate, btn.src); }
+        else if (cateParent === cates[2]) { cliquerTshirt(indexDansCate, btn.src); }
+        else if (cateParent === cates[3]) { cliquerAccessoire(btn.src); }
 
     });
 });
 
-// ---- SAUVEGARDE ----
+// =============================
+// SAUVEGARDE ET REDIRECTION
+// =============================
 const btnAchat = document.getElementById("btn-achat");
 const nomInput = document.getElementById("nom-perso");
 const messagePret = document.getElementById("message-pret");
@@ -315,7 +305,12 @@ btnAchat.addEventListener("click", (e) => {
         tshirtTop: document.getElementById("calque-tshirt").style.top,
         tshirtLeft: document.getElementById("calque-tshirt").style.left,
         tshirtWidth: document.getElementById("calque-tshirt").style.width,
-        
+
+        bras: document.getElementById("calque-bras").src,
+        brasTop: document.getElementById("calque-bras").style.top,
+        brasLeft: document.getElementById("calque-bras").style.left,
+        brasWidth: document.getElementById("calque-bras").style.width,
+
         accessoire: document.getElementById("calque-accessoire").src,
         accessoireTop: document.getElementById("calque-accessoire").style.top,
         accessoireLeft: document.getElementById("calque-accessoire").style.left,
