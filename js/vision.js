554be17a40f33c67e43834e3fea1e1650cad5771
Gaspar <gaspar.deutsch@gmail.com>
2026-04-22 19:18:08 +0200

    cleanups

--- js/vision.js
+++ js/vision.js
@@ -1,12 +1,38 @@
-// exemple
-function showStep(step) {
-  gsap.fromTo(document.querySelector("#step" + step), {
+const chemin = document.querySelector("#chemin-gsap")
+
+const longueurChemin = chemin.getTotalLength()
+
+gsap.fromTo(chemin, {
+    strokeDasharray: longueurChemin,
+    strokeDashoffset: longueurChemin
+}, {
+    strokeDashoffset: 0,
+    duration: 8,
+    ease: "Power2.out"
+})
+
+
+
+gsap.fromTo(document.querySelector("#marc-img"), {
     opacity: 0,
-    x: 1200
-  }, {
-    duration: 0.5,
-    ease: "power4.out",
-    x: 0,
+}, {
+    duration: 2,
+    ease: "power1.out",
     opacity: 1,
-  });
-}
+});
+
+gsap.fromTo(document.querySelector("#tiph-img"), {
+    opacity: 0,
+}, {
+    duration: 2,
+    ease: "power1.out",
+    opacity: 1,
+});
+
+gsap.fromTo(document.querySelector("#gaspar-img"), {
+    opacity: 0,
+}, {
+    duration: 2,
+    ease: "power1.out",
+    opacity: 1,
+});
