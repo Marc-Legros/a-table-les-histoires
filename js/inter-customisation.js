0a7b2194747b8c49f4c5ddb51cd725f9b58dd01d
Marc Legros <auflo2007@gmail.com>
2026-04-28 16:42:20 +0200

    Update z-index for accessory positioning based on character data

--- js/inter-customisation.js
+++ js/inter-customisation.js
@@ -92,7 +92,7 @@ const personnage = JSON.parse(localStorage.getItem("personnage"));
         accessoire.style.left = personnage.accessoireLeft;
         accessoire.style.width = personnage.accessoireWidth;
         accessoire.style.transform = personnage.accessoireTransform;
-        accessoire.style.zIndex = "-2";
+        accessoire.style.zIndex = personnage.accessoireZIndex;
     }
         // On remet le bras gauche
     const brasGauche = document.getElementById("inter-bras-gauche");
